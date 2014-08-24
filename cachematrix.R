40bd30e83d21cf21f753293e13b4c6bdf364065c
xiaotian27 <xiaotianjob@gmail.com>
2014-08-24 15:11:41 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function is used to create an invertible matrix object.
+## 
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      s<-NULL
+      set<-function(y){
+        x<<-y
+        s<<-NULL
+      }
+      get<-function() x
+      setsolve<-function(solve) s<<=solve
+      getsolve<-function() s
+      list(set=set,get=get,setsolve=setsolve,getsolve=getsolve)
+      
 }
 
 
-## Write a short comment describing this function
+## Use if.else loop to write the function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     s=s$getsolve()    ##
+     if(is.null(s)){
+        data<-x$get()
+        s=solve(data)
+        x$setsolve(s)
+        s
+     }else{
+          message("get the inverse of the matrix")
+          return(s)
+     }
 }
